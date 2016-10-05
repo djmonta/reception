7dd7f4054c1694c45294025aa69e277c92b7622a
monta <djmonta@gmail.com>
2016-10-05 23:23:54 +0900

    first commit

--- reception/ViewController.swift
+++ reception/ViewController.swift
@@ -10,16 +10,121 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var waitLabel: UILabel!
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        waitLabel.isHidden = true
+        waitLabel.alpha = 1
+        fadeOutTimer()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func pushButton(_ sender: AnyObject) {
+        if timer.isValid == true {
+            timer.invalidate()
+        }
+        waitLabel.fadeOut(type: FadeType.Normal)
+        
+        // create the url-request
+        let urlString = "https://api.instapush.im/v1/post"
+        let request = NSMutableURLRequest(url: NSURL(string: urlString)! as URL)
+        
+        // set the method(HTTP-POST)
+        request.httpMethod = "POST"
+        // set the header(s)
+        request.addValue("57f2fc98a4c48a2a482077c4", forHTTPHeaderField: "x-instapush-appid")
+        request.addValue("dd543bc65b4e0973264823e678d5915f", forHTTPHeaderField: "x-instapush-appsecret")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // set the request-body(JSON)
+        let params = [
+            "event": "coming",
+            "trackers": [
+                "name":"お客様"
+            ]
+        ] as [String : Any]
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: params, options: JSONSerialization.WritingOptions.prettyPrinted)
+        } catch let erro as NSError {
+            print(erro.localizedDescription)
+        }
+        
+        // use NSURLSessionDataTask
+        let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: {data, response, e in
+            if (e == nil) {
+                DispatchQueue.main.async {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
+                        // print(json)
+                        if json["error"] as? Error == nil {
+                            print("success")
+                            self.waitLabel.fadeIn(type: FadeType.Normal)
+                            self.fadeOutTimer()
+                        }
+                    } catch let err as NSError {
+                        print(err.localizedDescription)
+                    }
+                }
+            } else {
+                print(e?.localizedDescription)
+            }
+        })
+        task.resume()
+    }
+    
+    func waitLabelfadeOut() {
+        self.waitLabel.fadeOut(type: FadeType.Slow)
+    }
+    
+    func fadeOutTimer() {
+        self.timer = Timer.scheduledTimer(timeInterval: 29.0, target: self, selector: #selector(ViewController.waitLabelfadeOut), userInfo: nil, repeats: false)
+    }
 
+}
 
+enum FadeType: TimeInterval {
+    case
+    Normal = 0.2,
+    Slow = 1.0
+}
+
+extension UIView {
+    func fadeIn(type: FadeType = .Normal, completed: (() -> ())? = nil) {
+        fadeIn(duration: type.rawValue, completed: completed)
+    }
+    
+    /** For typical purpose, use "public func fadeIn(type: FadeType = .Normal, completed: (() -> ())? = nil)" instead of this */
+    func fadeIn(duration: TimeInterval = FadeType.Slow.rawValue, completed: (() -> ())? = nil) {
+        isHidden = false
+        alpha = 0
+        UIView.animate(withDuration: duration,
+                                   animations: {
+                                    self.alpha = 1
+        }) { finished in
+            completed?()
+        }
+    }
+    func fadeOut(type: FadeType = .Normal, completed: (() -> ())? = nil) {
+        fadeOut(duration: type.rawValue, completed: completed)
+    }
+    /** For typical purpose, use "public func fadeOut(type: FadeType = .Normal, completed: (() -> ())? = nil)" instead of this */
+    func fadeOut(duration: TimeInterval = FadeType.Slow.rawValue, completed: (() -> ())? = nil) {
+        UIView.animate(withDuration: duration
+            , animations: {
+                self.alpha = 0
+        }) { [weak self] finished in
+            self?.isHidden = true
+            self?.alpha = 1
+            completed?()
+        }
+    }
 }
 
